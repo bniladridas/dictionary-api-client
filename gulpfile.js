042bf394e0712cf5e818ef54310d0c1f543d4b2d
bniladridas <your_github_username@users.noreply.github.com>
2025-02-13 23:15:11 +0530

    Update gulpfile.js to signal async completion (8)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 
-gulp.task('default', function() {
+gulp.task('default', function(done) {
   // place code for your default task here
   console.log('Gulp is running!');
+  done(); // signal async completion
 });
